24d6012639be030851dcd142b307b40f97707aaa
Gitam <gitam.shimoni@gmail.com>
2023-09-22 00:27:15 +0300

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import AvailabilityTable from './Components/Worker/AvailabilityTable'
+
+function App() {
+
+  return (
+    <>
+        <AvailabilityTable></AvailabilityTable>
+    </>
+  )
+}
+
+export default App
